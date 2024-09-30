1b8126c4276732840b9da777a9ac0e6aeb525b3e
Kevin Kieninger <ktkienin@gmail.com>
2024-09-30 16:30:01 -0400

    chore: add prettier and format

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 // Global meta data
-export const SITE_TITLE = 'Kevin Kieninger';
-export const SITE_DESCRIPTION = 'Frontend software engineer based in Detroit, MI.';
+export const SITE_TITLE = "Kevin Kieninger";
+export const SITE_DESCRIPTION =
+  "Frontend software engineer based in Detroit, MI.";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				gray: {
-					100: '#fdfdfc', // light-bg
-					200: '#f9f9f8', // light-bg-hover
-					300: '#f1f0ef',
-					400: '#eeeeec', // dark-text-primary
-					500: '#b5b3ad', // dark-text-secondary
-					600: '#63635e', // light-text-secondary
-					700: '#21201c', // light-text-primary
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: "#fdfdfc", // light-bg
+          200: "#f9f9f8", // light-bg-hover
+          300: "#f1f0ef",
+          400: "#eeeeec", // dark-text-primary
+          500: "#b5b3ad", // dark-text-secondary
+          600: "#63635e", // light-text-secondary
+          700: "#21201c", // light-text-primary
 
-					800: '#191918', // dark-bg-hover
-					900: '#111110', // dark-bg
-				},
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+          800: "#191918", // dark-bg-hover
+          900: "#111110", // dark-bg
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
